04ce3cbc5358476e610c1965adc91e166a7db6e3
israel <adigun.jolly@gmail.com>
2022-03-24 18:24:15 +0100

    Created Loader Component

---
+++ src/Components/Loader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Loader = () => {
+    return (
+        <div className="loader-container">
+             <div className="loader-track">
+                <div className="loader-bar"></div>
+            </div>
+        </div>
+    )
+}
