b511442bd32ac3e5df0c4e2b1b4a0ddeee23ef5a
israel <adigun.jolly@gmail.com>
2022-03-24 18:09:34 +0100

    Enlarged history view created

--- src/Components/CalcDashboard.js
+++ src/Components/CalcDashboard.js
@@ -1,16 +1,40 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "../css/dashboard.css";
+import { ThemeContext } from "../Context/Theme/ThemeProvider";
 
 const CalcDashboard = () => {
-
+    const { theme } = useContext(ThemeContext);
+    const [opened, setOpened] = useState(false);
+    
+    const toggle = () => {
+        console.log(opened);
+        const dashboard = document.getElementById("dashboard");
+        if(!opened){
+            dashboard.classList.add("open")
+        }
+        else{
+            dashboard.classList.remove("open")
+        }
+        setOpened(prev => !prev);
+    }
     return (
-        <div className="dashboard dashboard-light">
+        <div className={`${theme.components.dashboard.body} dashboard`} id="dashboard" onClick={toggle}>
+            <div className="inner-dash-panel">
+                <div className="view-text">Click this window to view history</div>
+                <RegularDashboard />
+            </div>
         </div>
     )
 }
 
-const StandardDashboard = () => {}
+const RegularDashboard = () => {
+    return (
+        <div className="vh-100 d-flex justify-content-center align-items-center">
+            <p className="lead">Sike...No Content</p>
+        </div>
+    )
+}
 
-const ScientificDashboard = () => {}
+const CurrencyDashboard = () => {}
 
 export default CalcDashboard
