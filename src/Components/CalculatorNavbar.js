b2a30f774640f11b56818fdd48c69866e0ff1b22
israel <adigun.jolly@gmail.com>
2022-03-24 18:09:59 +0100

    Modified nav into dropdown

--- src/Components/CalculatorNavbar.js
+++ src/Components/CalculatorNavbar.js
@@ -1,49 +1,58 @@
 import React, { useState, useContext } from "react";
-import { ModeContext } from "../Context/Mode/ModeContext";
+import { ModeContext } from "../Context/Mode/ModeProvider";
+import { ThemeContext } from "../Context/Theme/ThemeProvider";
+import ThemeToggle from "./ThemeToggle";
 import "../css/navbar.css";
 
 const CalculatorNavbar = () => {
     const [toggled, setToggled] = useState(false);
 
     const { mode, changeMode } = useContext(ModeContext);
+
+    const { theme } = useContext(ThemeContext);
     
     const toggleMenu = () => {
-        const menu = document.getElementById("sliding-btn");
+        const btn = document.getElementById("sliding-btn");
+        const menu = document.getElementById("sliding-menu");
 
         if(toggled){
-            menu.classList.remove("open");
+            btn.classList.remove("open");
+            menu.classList.remove("open")
         }
         else{
+            btn.classList.add("open");
             menu.classList.add("open");
         }
-
-
         setToggled(prev => !prev);
+    }
+
+    const handleChange = (str="") => {
+        changeMode(str)
+        toggleMenu()
     }
     return (
         <>
             <div>
                 <div className="d-flex justify-content-between mx-4">
                     <div className=" navbar navbar-expand">
-                        <div className="menu-btn-light mx-2" id="sliding-btn" onClick={toggleMenu}></div>
-                        <div className="cal-type-light mx-3">{ mode.type }</div>
+                        <div className={`mx-2 ${theme.components.navbar.menubar}`} id="sliding-btn" onClick={toggleMenu}></div>
+                        <div className={`mx-3 ${theme.components.navbar.calType}`}>{ mode.type }</div>
                     </div>
-                <button className="help-btn-light">?</button>
+                    <div className="d-flex jusify-content-between align-items-center">
+                        <ThemeToggle />
+                        <button className={`mx-2 ${theme.components.navbar.help}`}>?</button>
+                    </div>
                 </div>
             </div>
-            { toggled ? 
-                <div className="sliding-menu-light">
-                    <nav className="w-50 mx-auto">
-                        <ul className="navbar-nav">
-                            <li className="nav-item text-center p-4 pointer"><div onClick={() => changeMode()}>Standard</div></li>
-                            <li className="nav-item text-center p-4 pointer"><div  onClick={() => changeMode("sci")}>Scientific</div></li>
-                            <li className="nav-item text-center p-4 pointer"><div  onClick={() => changeMode("curr")}>Currency Converter</div></li>
-                        </ul>
-                    </nav>
-                </div>
-                : 
-                <></>
-            }
+            <div className={`${theme.components.navbar.slidingMenu} sliding-menu`} id="sliding-menu">
+                <nav className="w-75 mx-auto">
+                    <ul className="navbar-nav">
+                        <li className="nav-item text-center p-4 pointer"><div onClick={() => handleChange()}>Standard</div></li>
+                        <li className="nav-item text-center p-4 pointer"><div  onClick={() => handleChange("sci")}>Scientific</div></li>
+                        <li className="nav-item text-center p-4 pointer"><div  onClick={() => handleChange("curr")}>Currency Converter</div></li>
+                    </ul>
+                </nav>
+            </div>
         </>
     )
 }
