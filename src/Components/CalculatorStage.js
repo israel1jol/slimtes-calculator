f88ee5835067cce1eb272558b640ce9f5e837313
israel <adigun.jolly@gmail.com>
2022-03-24 18:23:47 +0100

    Added Dynamic theme provider

--- src/Components/CalculatorStage.js
+++ src/Components/CalculatorStage.js
@@ -1,17 +1,19 @@
-import React, { useContext, useState } from "react";
-import { ModeContext } from "../Context/Mode/ModeContext";
+import React, { useContext } from "react";
+import { ModeContext } from "../Context/Mode/ModeProvider";
+import { ThemeContext } from "../Context/Theme/ThemeProvider";
 import CalculatorNavbar from "./CalculatorNavbar";
 import CalcDashboard from "./CalcDashboard";
 import CalculatorScene from "./CalculatorScene";
 import Panel from "./Panel";
-
+import "../css/stage.css"
 
 
 const CalculatorStage = () => {
     const { mode, changeMode } = useContext(ModeContext);
+    const { theme } = useContext(ThemeContext);
 
     return (
-        <div className="stage">
+        <div className={`stage ${ theme.components.stage.body }`}>
             <CalculatorNavbar />
             <CalculatorScene>
                 <CalcDashboard mode={mode}/>
