c42deecaff2ed227f13c6276486491b52969b14e
israel <adigun.jolly@gmail.com>
2022-03-24 18:25:06 +0100

    Added Dynamic Theme classes

--- src/Components/PanelFrame.js
+++ src/Components/PanelFrame.js
@@ -1,144 +1,88 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useStandardButtonSet, useScientificButtonSet } from "../Hooks/ButtonSet";
 import "../css/Frame.css";
 
-export const StandardPanelFrame = () => {
+export const StandardPanelFrame = ({theme}) => {
     const [UpperBound, firstButtonSet, secondButtonSet, thirdButtonSet, fourthButtonSet, fifthButtonSet, sixthButtonSet] = useStandardButtonSet();
     return (
-        <div className="frame">
+        <div className={`frame ${theme}`}>
             <div className="standard-upperbound-grid">
-                {
-                    UpperBound.map((button, id) => (
-                        <li key={id} className="button-item">
-                            <button className={`${button.type}`}>{button.name}</button>
-                        </li>
-                    ))
-                }
+                {UpperBound.map((button, id) => ( 
+                    <li key={id} className="button-item"><button className={`${button.type}`}>{button.name}</button></li>
+                    )) }
             </div>
             <div className="standard-button-grid">
-                {
-                    firstButtonSet.map((button, id) => (
-                        <li key={id} className="button-item">
-                            <button className={`${button.type}`}>{button.name}</button>
-                        </li>
-                    ))
-                }
+                { firstButtonSet.map((button, id) => ( 
+                    <li key={id} className="button-item"><button className={`${button.type}`}>{button.name}</button></li>
+                    ))}
             </div>
             <div className="standard-button-grid">
-                {
-                    secondButtonSet.map((button, id) => (
-                        <li key={id} className="button-item">
-                            <button className={`${button.type}`}>{button.name}</button>
-                        </li>
-                    ))
-                }
+                {secondButtonSet.map((button, id) => (
+                    <li key={id} className="button-item"><button className={`${button.type}`}>{button.name}</button></li>
+                    ))}
             </div>
             <div className="standard-button-grid">
-                {
-                    thirdButtonSet.map((button, id) => (
-                        <li key={id} className="button-item">
-                            <button className={`${button.type}`}>{button.name}</button>
-                        </li>
-                    ))
-                }
+                { thirdButtonSet.map((button, id) => (
+                        <li key={id} className="button-item"><button className={`${button.type}`}>{button.name}</button></li>
+                    ))}
             </div>
             <div className="standard-button-grid">
-                {
-                    fourthButtonSet.map((button, id) => (
-                        <li key={id} className="button-item">
-                            <button className={`${button.type}`}>{button.name}</button>
-                        </li>
-                    ))
-                }
+                {fourthButtonSet.map((button, id) => (
+                        <li key={id} className="button-item"><button className={`${button.type}`}>{button.name}</button></li>
+                    ))}
             </div>
             <div className="standard-button-grid">
-                {
-                    fifthButtonSet.map((button, id) => (
-                        <li key={id} className="button-item">
-                            <button className={`${button.type}`}>{button.name}</button>
-                        </li>
-                    ))
-                }
+                {fifthButtonSet.map((button, id) => (
+                        <li key={id} className="button-item"><button className={`${button.type}`}>{button.name}</button></li>
+                    ))}
             </div>
             <div className="standard-heavy-grid">
-                {
-                    sixthButtonSet.map((button, id) => (
-                        <li key={id} className="button-item">
-                            <button className={`${button.type}`}>{button.name}</button>
-                        </li>
-                    ))
-                }
+                {sixthButtonSet.map((button, id) => (
+                        <li key={id} className="button-item"><button className={`${button.type}`}>{button.name}</button></li>
+                    ))}
             </div>
         </div>
     )
 }
 
-export const ScientificPanelFrame = () =>{
+export const ScientificPanelFrame = ({theme}) =>{
     const [UpperBound, firstButtonSet, secondButtonSet, thirdButtonSet, fourthButtonSet, fifthButtonSet, sixthButtonSet] = useScientificButtonSet();
     return (
-        <div className="frame">
+        <div className={`frame ${theme}`}>
             <div className="scientific-upperbound-grid">
-                {
-                    UpperBound.map((button, id) => (
-                        <li key={id} className="button-item">
-                            <button className={`${button.type}`}>{button.name}</button>
-                        </li>
-                    ))
-                }
+                {UpperBound.map((button, id) => (
+                        <li key={id} className="button-item"><button className={`${button.type}`}>{button.name}</button></li>
+                    ))}
             </div>
             <div className="scientific-upperbound-grid">
-                {
-                    firstButtonSet.map((button, id) => (
-                        <li key={id} className="button-item">
-                            <button className={`${button.type}`}>{button.name}</button>
-                        </li>
-                    ))
-                }
+                {firstButtonSet.map((button, id) => (
+                        <li key={id} className="button-item"><button className={`${button.type}`}>{button.name}</button></li>
+                    ))}
             </div>
             <div className="scientific-upperbound-grid">
-                {
-                    secondButtonSet.map((button, id) => (
-                        <li key={id} className="button-item">
-                            <button className={`${button.type}`}>{button.name}</button>
-                        </li>
-                    ))
-                }
+                {secondButtonSet.map((button, id) => (
+                        <li key={id} className="button-item"><button className={`${button.type}`}>{button.name}</button></li>
+                    ))}
             </div>
             <div className="scientific-upperbound-grid">
-                {
-                    thirdButtonSet.map((button, id) => (
-                        <li key={id} className="button-item">
-                            <button className={`${button.type}`}>{button.name}</button>
-                        </li>
-                    ))
-                }
+                {thirdButtonSet.map((button, id) => (
+                        <li key={id} className="button-item"><button className={`${button.type}`}>{button.name}</button></li>
+                    ))}
             </div>
             <div className="scientific-upperbound-grid">
-                {
-                    fourthButtonSet.map((button, id) => (
-                        <li key={id} className="button-item">
-                            <button className={`${button.type}`}>{button.name}</button>
-                        </li>
-                    ))
-                }
+                {fourthButtonSet.map((button, id) => (
+                        <li key={id} className="button-item"><button className={`${button.type}`}>{button.name}</button></li>
+                    ))}
             </div>
             <div className="scientific-upperbound-grid">
-                {
-                    fifthButtonSet.map((button, id) => (
-                        <li key={id} className="button-item">
-                            <button className={`${button.type}`}>{button.name}</button>
-                        </li>
-                    ))
-                }
+                {fifthButtonSet.map((button, id) => (
+                        <li key={id} className="button-item"><button className={`${button.type}`}>{button.name}</button></li>
+                    ))}
             </div>
             <div className="scientific-heavy-grid">
-                {
-                    sixthButtonSet.map((button, id) => (
-                        <li key={id} className="button-item">
-                            <button className={`${button.type}`}>{button.name}</button>
-                        </li>
-                    ))
-                }
+                {sixthButtonSet.map((button, id) => (
+                        <li key={id} className="button-item"><button className={`${button.type}`}>{button.name}</button></li>
+                    ))}
             </div>
         </div>
     )
